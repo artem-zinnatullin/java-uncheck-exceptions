db84a44e9a4b6362d8e158249030ca989cb36900
Artem Zinnatullin :slowpoke <artem.zinnatullin@gmail.com>
2018-04-19 05:31:20 -0700

    Add README and switch to static import. (#4)

--- uncheck-exceptions-integration-test/src/test/java/com/artemzin/uncheckexceptions/integrationtest/UncheckExceptionsIntegrationTest.java
+++ uncheck-exceptions-integration-test/src/test/java/com/artemzin/uncheckexceptions/integrationtest/UncheckExceptionsIntegrationTest.java
@@ -1,15 +1,15 @@
 package com.artemzin.uncheckexceptions.integrationtest;
 
-import com.artemzin.uncheckexceptions.UncheckExceptions;
 import org.junit.Test;
 
+import static com.artemzin.uncheckexceptions.UncheckExceptions.thr0w;
 import static org.junit.Assert.assertSame;
 
 public class UncheckExceptionsIntegrationTest {
 
     @Test(expected = Exception.class)
     public void thr0wDoesNotRequireExceptionInMethodSignature() {
-        UncheckExceptions.thr0w(new Exception());
+        thr0w(new Exception());
     }
 
     private static final class TestCheckedException extends Exception {
@@ -21,7 +21,7 @@ public class UncheckExceptionsIntegrationTest {
         TestCheckedException testException = new TestCheckedException();
 
         try {
-            UncheckExceptions.thr0w(testException);
+            thr0w(testException);
         } catch (Throwable expected) { // Loooool, compiler doesn't let us declare expected exception as TestCheckedException because it doesn't see it in method description xD
             assertSame(testException, expected);
         }
